69602f751aeb7fa0cfa984b77d6d59b938cce092
Ramon Blanquer <ramon@ramonblanquer.com>
2014-03-21 10:04:49 +0000

    WIP files uploaded

--- script/LS_string_rewriting.py
+++ script/LS_string_rewriting.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 '''
-    L-System String Rewriting Module:    "LS_string_rewriting.py"
+    L-System String Rewriting Module:    <LS_string_rewriting.py>
+
+    This module will generate a text string which later will  be used by the interpreter module to
+    generate all the geometry according to certain conventions.
+
+    Fix issues:
+        - The probabilities are forced to be integers, a way to operate with float could improve it.
 '''
 
 import maya.cmds as cmds
@@ -10,50 +16,91 @@ import copy
 import LS_interpreter
 reload(LS_interpreter)
 
-def writeLS(pW, pP, pDepth, replacingDone):
+def writeLS(pW, pP, pDepth):
 
-    """ Iterates through the string. It will create a global string variable called "result" which is a
+    ''' Iterates through the string. It will create a global string variable called "result" which is a
         concatenation of various additions done when reading the Axiom or Word entered by the user (W).
 
         pW :      Axiom, the initial state.
-        pP :      Production rules, set as a 2-item tuple. The first one indicates the condition and the
-                  second one the value the first item should be replaced with.
-        pDepth :  Recursive index.
+        pP:       Number of production rules. Production rules are used to replace letters by another
+                  sequence of them. For instance 'F --> F+F-F' means if you find an F, replace it by
+                  F+F-F. You don't need to know the meaning of the symbols yet, just get the idea.
+                  LSystems on their own are not more than that, a set of rules which iterate recursively.
+                  They are set as a 3-item tuple. The first one indicates the probability for this rule
+                  to apply. The second one is the letter that must be replaced and the third one, the
+                  content to be replaced with. (We can have multiple rules which apply for a same
+                  letter, that's why we take into account the percentages --> Stochastic).
+        pDepth :  Recursive index or number of iterations over the string.
+
         On Exit : Will return a result string. Thus it is recommendable binding the call to a variable.
 
         For example:
-            >>> W = 'F'                           # The initial word. It is properly called Axiom.
-            >>> P = ['F', 'F[+F]F[-F]F']          # Means: When you find 'F', replace it with
-            >>> depth = 1                         # Depth of the procedure, number of iterations
-            >>> variable1 = writeLS(W, P, depth)   # Binds the return value to a variable...
+            >>> W = 'F'                            # The initial word. It is properly called Axiom
+            >>> pPNum = 1                          # We will have just one production rule
+            >>> P = [100, 'F', 'F[+F]F[-F]F']      # Means: ALWAYS when you find 'F', replace it with ~
+            >>> depth = 1                          # Depth of the procedure, number of iterations
+            >>> variable1 = writeLS(W, pPNum, P, depth) 
+                                                   # Binds the return value to a variable...
             >>> print variable1                    # ... which now is printed
             F[+F]F[-F]F
             >>>
 
+        As we have seen here above ^ we started with an initial axiom F, and it has been replaced
+        following a production rule.
+
         If the depth increases we carry the recursion one step further:
-            >>> W = 'F'                           # The initial word. It is properly called Axiom.
-            >>> P = ['F', 'F[+F]F[-F]F']          # Means: When you find 'F', replace it with
-            >>> depth = 2                         # Depth of the procedure, number of iterations
-            >>> variable2 = writeLS(W, P, depth)   # Binds the return value to a variable...
-            >>> print variable2                    # ... which now is printed
+            >>> W = 'F'
+            >>> pPNum = 1
+            >>> P = [100, 'F', 'F[+F]F[-F]F']
+            >>> depth = 2
+            >>> variable2 = writeLS(W, pPNum, P, depth)
+            >>> print variable2
             F[+F]F[-F]F[+F[+F]F[-F]F]F[+F]F[-F]F[-F[+F]F[-F]F]F[+F]F[-F]F
             >>>
 
-        This last example would be the same as we run writeLS(variable1, P, depth), that is to say,
+        This last example would be the same as we run writeLS(variable1, pPNum, P, depth), that is to say,
         it is the same as running again the procedure over the generated string in the previous example.
-    """
+    '''
+    if pDepth == 0:
+        return pW
+
+    else:
+        tempList = ''
+        coincidence = []
+        for i in range(0, len(pW)):    # Search for coincidences in the string
+            for j in range(0, len(pP)):
+                if pW[i] == pP[j]:
+                    coincidence.append([i,j])    # Appends the index of the string in which the rule applies and the rule index
+
+            if len(coincidence) == 0:    # If coincidence is empty
+                pass
+            elif len(coincidence) == 1:
+                pW.replace(coincidence[0][0],pP[coincidence[0][1])
+            else:
+                tempList += pW[:coincidence[0][0]] # Add tp the temp list what we have before the 1st coincidence
+                choiceList = []
+                for item in coincidence:
+                    choiceList.append(int(pP[0])*[pP[j][1],pP[j][2]])
+                pickedRule = random.choice(choiceList)
+                tempList += pickedRule[1]
+        pW = tempList
+        return writeLS(pW, pP, pDepth-1)
+
+
+'''
     if pDepth == 0:
         return pW
     else:
         tempList = ''
         for i in range(0,len(pW)):
             for j in range(0,len(pP)):
-                if pW[i] == pP[j][0]:
-                    tempList += pP[j][1]
+                if pW[i] == pP[j][1]:
+                    tempList += pP[j][2]
                     replacingDone = True
                     break
             if replacingDone == False:
                 tempList += pW[i]
             replacingDone = False
         pW = tempList
-        return writeLS(pW, pP, pDepth-1, replacingDone)
+        return 'writeLS(pW, pPNum, pP, pDepth-1, replacingDone)'
+'''
--- script/startScript.py
+++ script/startScript.py
@@ -2,80 +2,79 @@
 
 ''' DISCLAIMER: This startScript.py file is adapted from Jared Auty's scripting project "L-System Plant Generator". All the
     copyrights belongs him, just on this file, of course.
-	 
-	 You can access his source code here: 
+     
+    You can access his source code here: 
 
-                    https://docs.google.com/file/d/0B75Ij6W0fkuidkxhY1ozanFzVEU/
+        https://docs.google.com/file/d/0B75Ij6W0fkuidkxhY1ozanFzVEU/
 
-   I recommend checking Jared Auty's webpage (jaredauty.com), he has been working in many
-   interesting projects.
+    I recommend checking Jared Auty's webpage (jaredauty.com), he has been working in many
+    interesting projects.
 
-	// 
+    // 
 
-   Start Up Script   <startScript.py>
+    Start Up Script    <startScript.py>
 
 Select everything and paste it into a new maya python scripting window. Then run it and follow the instructions
 '''
 
 import maya.cmds as cmds
 
-
 class findPathWindow:
-   '''
-   This class is responsible for managing the module-fininding window. This window is run when the script can't find the modules it needs.
-   It asks the user to locate the modules. The user-inputted path is then saved in python's temporary system paths. This means that next 
-   time it tries to load the modules it will look in the right path
-   '''
-   def __init__(self):
-     '''
-     This function initialises all the needed values and creates the GUI. It is run automatically when a new instance of the class is run.
-     '''
-     self.fileName = []
-     self.fileType = []
-     #---Create UI elements---#
-     self.loadDirWin = cmds.window(title='L-System Interpreter', sizeable=False)
-     self.loadDirForm = cmds.formLayout(numberOfDivisions=100)
-     self.loadDirColumns = cmds.columnLayout(parent=self.loadDirForm, adjustableColumn=True, rowSpacing=5)
-     self.loadDirText = cmds.text(parent=self.loadDirColumns, font='boldLabelFont', label='Welcome to Ramon\'s L-System Interpreter\n\nTo start using the script simply browse to the directory containing the modules and then press \'Continue\'\n\n|\n|\nv')
-     self.loadDirPath = cmds.textField(parent = self.loadDirColumns)
-     self.loadDirBrowse= cmds.button(parent = self.loadDirColumns, label='Browse', command=self.openFile)
-     self.loadDirLoad = cmds.button(parent = self.loadDirColumns, label ='Continue', command=self.appendPath)
-     #---Make sure the colums layout stretches with the window---#
-     cmds.formLayout(self.loadDirForm, edit=True, attachForm=[
-         (self.loadDirColumns, 'top', 20),
-         (self.loadDirColumns, 'left', 20),
-         (self.loadDirColumns, 'right', 20),
-         (self.loadDirColumns, 'bottom', 20),
-         ])
-   def appendPath(self, *args):
-      '''
-      This function queries the text field and puts the path in the system paths. It then runs the 'closeAll' function
-      '''
-      self.fileName = cmds.textField(self.loadDirPath, query=True, text=True) #query textfield and save in self.fileName
-      sys.path.append(self.fileName)   #append to system paths
-      if self.fileName: #If the fileName is not empty
-         self.closeAll()
+    '''
+    This class is responsible for managing the module-fininding window. This window is run when the script can't find the modules it needs.
+    It asks the user to locate the modules. The user-inputted path is then saved in python's temporary system paths. This means that next 
+    time it tries to load the modules it will look in the right path
+    '''
+    def __init__(self):
+        '''
+         This function initialises all the needed values and creates the GUI. It is run automatically when a new instance of the class is run.
+        '''
+        self.fileName = []
+        self.fileType = []
+        #---Create UI elements---#
+        self.loadDirWin = cmds.window(title='L-System Interpreter', sizeable=False)
+        self.loadDirForm = cmds.formLayout(numberOfDivisions=100)
+        self.loadDirColumns = cmds.columnLayout(parent=self.loadDirForm, adjustableColumn=True, rowSpacing=5)
+        self.loadDirText = cmds.text(parent=self.loadDirColumns, font='boldLabelFont', label='Welcome to Ramon\'s L-System Interpreter\n\nTo start using the script simply browse to the directory containing the modules and then press \'Continue\'\n\n|\n|\nv')
+        self.loadDirPath = cmds.textField(parent = self.loadDirColumns)
+        self.loadDirBrowse= cmds.button(parent = self.loadDirColumns, label='Browse', command=self.openFile)
+        self.loadDirLoad = cmds.button(parent = self.loadDirColumns, label ='Continue', command=self.appendPath)
+        #---Make sure the colums layout stretches with the window---#
+        cmds.formLayout(self.loadDirForm, edit=True, attachForm=[
+            (self.loadDirColumns, 'top', 20),
+            (self.loadDirColumns, 'left', 20),
+            (self.loadDirColumns, 'right', 20),
+            (self.loadDirColumns, 'bottom', 20),
+            ])
+    def appendPath(self, *args):
+        '''
+        This function queries the text field and puts the path in the system paths. It then runs the 'closeAll' function
+        '''
+        self.fileName = cmds.textField(self.loadDirPath, query=True, text=True)    # Query textfield and save in self.fileName
+        sys.path.append(self.fileName)    # Append to system paths
+        if self.fileName:    # If the fileName is not empty
+            self.closeAll()
       
-   def openFile(self, *args):
-      '''
-      This opens the file browser and takes the path and puts it in the text field. This is run when the Browse button is pressed.
-      '''
-      self.fileName = cmds.fileDialog2(fileMode=2, caption="Import Image") #Open the file browser
-      cmds.textField(self.loadDirPath, edit=True, text=str(self.fileName[0])) #put path in text field
+    def openFile(self, *args):
+        '''
+        This opens the file browser and takes the path and puts it in the text field. This is run when the Browse button is pressed.
+        '''
+        self.fileName = cmds.fileDialog2(fileMode=2, caption="Import Image")    # Open the file browser
+        cmds.textField(self.loadDirPath, edit=True, text=str(self.fileName[0]))    # Put path in text field
       
-   def closeAll(self, *args):
-      '''
-      This function closes the window and starts up the main GUI. This is run when the Continue button is pressed.
-      '''
-      cmds.deleteUI(self.loadDirWin, window=True)  #close window
-      gui.createUI()  #start UI
+    def closeAll(self, *args):
+        '''
+        This function closes the window and starts up the main GUI. This is run when the Continue button is pressed.
+        '''
+        cmds.deleteUI(self.loadDirWin, window=True)    # Close window
+        gui.createUI()    # Start UI
 
 
 class errorWindow:
-   '''
-   This class creates an error window that displays a message passed it at creation time.
-   '''
-   def __init__(self, message):
+    '''
+    This class creates an error window that displays a message passed it at creation time.
+    '''
+    def __init__(self, message):
         '''
         This function creates the window. This is run when a new instance of the class is created. When creating a new instance
         a string called message is passed in, this is then displayed on the window
@@ -87,31 +86,32 @@ class errorWindow:
         cmds.formLayout(self.form, edit=True,attachForm=[[self.text, 'left', 0], [self.text, 'right', 0], [self.okButton, 'left', 0], [self.okButton, 'right', 0]])
         cmds.formLayout(self.form, edit=True,attachPosition=[[self.text, 'top', 0, 0], [self.text, 'bottom', 0, 70], [self.okButton, 'top', 0, 70], [self.okButton, 'bottom', 0, 100]])
         cmds.showWindow(self.win)
-   def close(self, *args):
-      '''
-      This function closed the window and is run when the 'OK' button is run
-      '''
-      cmds.deleteUI(self.win)
+    def close(self, *args):
+        '''
+        This function closed the window and is run when the 'OK' button is run
+        '''
+        cmds.deleteUI(self.win)
 
-try:  #if a gui is already open close it
-   if cmds.window(win.window, exists=True):
-      cmds.deleteUI(win.window, window=True)
+try:    # If a gui is already open close it
+    if cmds.window(win.window, exists=True):
+        cmds.deleteUI(win.window, window=True)
 except:
-   pass
+    pass
 
-try:  #try to import all the modules and then start the UI
-   import maya.cmds as cmds
-   import random
-   import math
-   import copy
-   import LS_string_rewriting
-   reload(LS_string_rewriting)
-   import LS_interpreter
-   reload(LS_interpreter)
-   import gui
-   reload(gui)
-   gui.createUI()
-except:  #if the above fails create an instance of the findPathWindow to try to find the modules
-   import sys
-   load = findPathWindow()
-   cmds.showWindow(load.loadDirWin)
+try:    # Try to import all the modules and then start the UI
+    import maya.cmds as cmds
+    import random
+    import math
+    import copy
+    import LS_string_rewriting
+    reload(LS_string_rewriting)
+    import LS_interpreter
+    reload(LS_interpreter)
+    import gui
+    reload(gui)
+    gui.createUI()
+
+except:    # If the above fails create an instance of the findPathWindow to try to find the modules
+    import sys
+    load = findPathWindow()
+    cmds.showWindow(load.loadDirWin)
