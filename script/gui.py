4d652d0560adc2c75672ee40ca83d4721ac8b686
Ramon Blanquer <ramon@ramonblanquer.com>
2014-03-31 02:16:38 +0100

    Some formatting done.

--- script/gui.py
+++ script/gui.py
@@ -19,7 +19,8 @@ def createUI():
         cmds.deleteUI('myWindow')
 
     # Create our window
-    window = cmds.window('myWindow', title='L-System Interpreter', s=False, w=450, h=650, mnb=False, mxb=False)
+    window = cmds.window('myWindow', title='L-System Interpreter', s=False, w=450, h=650, mnb=False,
+        mxb=False)
 
     # Our layout will be 450 px width and 650 px height. I set an image as well.
     mainLayout = cmds.columnLayout(w=450, h=650)
@@ -27,7 +28,8 @@ def createUI():
     cmds.image(w=450, h=160, image=imagePath)
 
     #--- MAIN FRAME LAYOUT ---#
-    mainFrame = cmds.frameLayout(label = 'L-System String Operations', labelVisible=False, collapsable=False, cl=True, mw = 10, mh = 10)
+    mainFrame = cmds.frameLayout(label = 'L-System String Operations', labelVisible=False,
+        collapsable=False, cl=True, mw = 10, mh = 10)
 
     #--- Instructions ---#
     def displayInstructions(*pArgs):
@@ -35,7 +37,8 @@ def createUI():
             cmds.deleteUI('instructions_window')
         instructions_window = cmds.window('instructions_window', title='Instructins')
         cmds.showWindow(instructions_window)
-    cmds.rowColumnLayout( numberOfColumns=4, cal=[(2,'left')], columnWidth=[(1,5),(2,320),(3,100),(4,5)], parent=mainFrame)
+    cmds.rowColumnLayout( numberOfColumns=4, cal=[(2,'left')], columnWidth=[(1,5),(2,320),(3,100),(4,5)],
+        parent=mainFrame)
     cmds.separator(st='none')
     cmds.text(l='You might feel a bit lost, I recommend you to read a quick guide')
     cmds.button(l='Instrucions', en=True, ebg=True, w=90, command=displayInstructions)
@@ -51,26 +54,32 @@ def createUI():
 
     cmds.rowColumnLayout( numberOfColumns=2, columnWidth=[(1, 43), (2, 363)], parent=rulesLayout)
     cmds.text(l='Axiom ', align='right')
-    cmds.textField('axiomTextField', tx='F',ann='Type the initial word you want to start with. The initialiser.')
+    cmds.textField('axiomTextField', tx='F',
+        ann='Type the initial word you want to start with. The initialiser.')
         #--- Depth ---#
     cmds.rowColumnLayout( numberOfColumns=1, columnWidth=[(1, 406)], parent=rulesLayout)
-    cmds.intSliderGrp('depthIntField', l="Depth: ", v=3, cw3=[40,30,350], min=1, max=10, fmx=20, f=True, ann='Set the index of recursion. The number of iterations over the generated string.')
+    cmds.intSliderGrp('depthIntField', l="Depth: ", v=3, cw3=[40,30,350], min=1, max=10, fmx=20, f=True,
+        ann='Set the index of recursion. The number of iterations over the generated string.')
 
         #--- Probabilities header ---#
-    cmds.rowColumnLayout( numberOfColumns=3, cal=[(1,"right")], columnWidth=[(1,325),(2,50),(3,45)], parent=rulesLayout)
+    cmds.rowColumnLayout( numberOfColumns=3, cal=[(1,"right")], columnWidth=[(1,325),(2,50),(3,45)],
+        parent=rulesLayout)
     cmds.separator(st='none')
     cmds.text(l='Prob.(%)', en=True, ebg=True, bgc=[0.0,0.153,0.0666])
     cmds.separator(st='none')
         #--- Collection of rules ---#
 
-    cmds.rowColumnLayout( numberOfColumns=7, cal=[(1,"right")], columnWidth=[(1,40),(2,30),(3,20),(4,245),(5,40),(6,5),(7,20)], parent=rulesLayout)
+    cmds.rowColumnLayout( numberOfColumns=7, cal=[(1,"right")],
+        columnWidth=[(1,40),(2,30),(3,20),(4,245),(5,40),(6,5),(7,20)], parent=rulesLayout)
 
             #--- RULE 1 ---#
     cmds.text(l='Rule 1: ', en=True)
     cmds.textField('prodRulePred1', en=True, tx='F', ann='Enter predecessor string for production rule 1')
     cmds.text(l='->', en=True)
-    cmds.textField('prodRuleSucc1', en=True, tx='F[&+F]F[->F][&F]', ann='Enter successor string for production rule 1')
-    cmds.intField('prodRuleProb1', minValue=0, maxValue=100, value=100, ann='Enter the probability (in percentage %) in which you want this rule to be executed')
+    cmds.textField('prodRuleSucc1', en=True, tx='F[&+F]F[->F][&F]',
+        ann='Enter successor string for production rule 1')
+    cmds.intField('prodRuleProb1', minValue=0, maxValue=100, value=100,
+        ann='Enter the probability (in percentage %) in which you want this rule to be executed')
     cmds.separator(st='none')
     cmds.separator(st='none')
 
@@ -79,7 +88,8 @@ def createUI():
     cmds.textField('prodRulePred2', en=False, ann='Enter predecessor string for production rule 2')
     cmds.text('prodRule2Text_B', l='->', en=False)
     cmds.textField('prodRuleSucc2', en=False, ann='Enter successor string for production rule 2')
-    cmds.intField('prodRuleProb2', minValue=0, maxValue=100, value=0, en=False, ann='Enter the probability (in percentage %) in which you want this rule to be executed')
+    cmds.intField('prodRuleProb2', minValue=0, maxValue=100, value=0, en=False,
+        ann='Enter the probability (in percentage %) in which you want this rule to be executed')
     cmds.separator(st='none')
     cmds.checkBox('prodRuleCheckBox2', l='', value=False, ann='Activates the 2st production rule')
     def toggleGreyingOut2(*pArgs):
@@ -103,7 +113,8 @@ def createUI():
     cmds.textField('prodRulePred3', en=False, ann='Enter predecessor string for production rule 2')
     cmds.text('prodRule3Text_B', l='->', en=False)
     cmds.textField('prodRuleSucc3', en=False, ann='Enter successor string for production rule 3')
-    cmds.intField('prodRuleProb3', minValue=0, maxValue=100, value=0, en=False, ann='Enter the probability (in percentage %) in which you want this rule to be executed')
+    cmds.intField('prodRuleProb3', minValue=0, maxValue=100, value=0, en=False,
+        ann='Enter the probability (in percentage %) in which you want this rule to be executed')
     cmds.separator(st='none')
     cmds.checkBox('prodRuleCheckBox3', l='', value=False, ann='Activates the 3rd production rule')
     def toggleGreyingOut3(*pArgs):
@@ -127,7 +138,8 @@ def createUI():
     cmds.textField('prodRulePred4', en=False, ann='Enter predecessor string for production rule 4')
     cmds.text('prodRule4Text_B', l='->', en=False)
     cmds.textField('prodRuleSucc4', en=False, ann='Enter successor string for production rule 4')
-    cmds.intField('prodRuleProb4', minValue=0, maxValue=100, value=0, en=False, ann='Enter the probability (in percentage %) in which you want this rule to be executed')
+    cmds.intField('prodRuleProb4', minValue=0, maxValue=100, value=0, en=False,
+        ann='Enter the probability (in percentage %) in which you want this rule to be executed')
     cmds.separator(st='none')
     cmds.checkBox('prodRuleCheckBox4', l='', value=False, ann='Activates the 4th production rule')
     def toggleGreyingOut4(*pArgs):
@@ -148,36 +160,46 @@ def createUI():
 
         #--- Generate String / Clear String  ---#
     cmds.rowColumnLayout( numberOfColumns=3, columnWidth=[(1,208), (2,10), (3,208)], parent=rulesLayout)
-    cmds.button(l='Generate String', ann='Click to run the L-System string calculation', command=generateStringButtonAction)
+    cmds.button(l='Generate String', ann='Click to run the L-System string calculation',
+        command=generateStringButtonAction)
     cmds.separator(h=5, st='none')
-    cmds.button(l='Clear String', ann='Click to reset the generated string', command=clearStringButtonAction)
+    cmds.button(l='Clear String', ann='Click to reset the generated string',
+        command=clearStringButtonAction)
 
         #--- String Output ---#
     cmds.rowColumnLayout( numberOfColumns=1, columnWidth=[(1, 426)], parent=rulesLayout)
-    cmds.textField("output", editable=False, ann='This is the generated string. Now Proceed to build the geometry.')
+    cmds.textField("output", editable=False,
+        ann='This is the generated string. Now Proceed to build the geometry.')
 
     cmds.rowColumnLayout( numberOfColumns=1, columnWidth=[(1, 426)], parent=mainFrame)
     cmds.separator(h=5, st='none')
 
     #--- GEOMETRIC INTERPRETATION LAYOUT ---#
-    mInterpret = cmds.frameLayout(label = "Geometric Interpretation", collapsable=True, cl=True, mw = 10, mh = 10)
+    mInterpret = cmds.frameLayout(label = "Geometric Interpretation", collapsable=True, cl=True, mw = 10,
+        mh = 10)
  
         #--- Set of geometry parameters ---#
     cmds.rowColumnLayout( numberOfColumns=1, columnWidth=[(1,406)], parent=mInterpret)
-    cmds.floatSliderGrp("angle", l="Angle: ", pre=1, v=28, cal=[2,'center'], cw3=[92,40,788], min=0, max=100, fmx=360, f=True)
-    cmds.floatSliderGrp("length", l="Segment Length: ", pre=2, v=1.2, cw3=[92,40,788], min=0, max=10, fmx=100, f=True)
-    cmds.floatSliderGrp("radius", l="Segment Radius: ", pre=2, v=0.2, cw3=[92,40,788], min=0, max=0.5, fmx=2, f=True)
-    cmds.intSliderGrp("cylSubdivs", l="Cylinder Subdivs: ", v=8, cw3=[92,40,788], min=4, max=20, fmx=20, f=True)
-    cmds.floatSliderGrp("length_atenuation", l="Length Atenuation: ", v=1, cw3=[92,40,788], min=0, max=1, fmx=1, f=True, pre=2, ann='This number will multiply the length of the next segment, recursively')
-    cmds.floatSliderGrp("radius_atenuation", l="Radius Atenuation: ", v=1, cw3=[92,40,788], min=0, max=1, fmx=1, f=True, pre=2, ann='This number will multiply the length of the next segment, recursively')
+    cmds.floatSliderGrp("angle", l="Angle: ", pre=1, v=28, cal=[2,'center'], cw3=[92,40,788], min=0,
+        max=100, fmx=360, f=True)
+    cmds.floatSliderGrp("length", l="Segment Length: ", pre=2, v=1.2, cw3=[92,40,788], min=0, max=10,
+        fmx=100, f=True)
+    cmds.floatSliderGrp("radius", l="Segment Radius: ", pre=2, v=0.2, cw3=[92,40,788], min=0, max=0.5,
+        fmx=2, f=True)
+    cmds.intSliderGrp("cylSubdivs", l="Cylinder Subdivs: ", v=8, cw3=[92,40,788], min=4, max=20, fmx=20,
+        f=True)
+    cmds.floatSliderGrp("length_atenuation", l="Length Atenuation: ", v=1, cw3=[92,40,788], min=0, max=1,
+        fmx=1, f=True, pre=2, ann='This number will multiply the length of the next segment, recursively')
+    cmds.floatSliderGrp("radius_atenuation", l="Radius Atenuation: ", v=1, cw3=[92,40,788], min=0, max=1,
+        fmx=1, f=True, pre=2, ann='This number will multiply the length of the next segment, recursively')
 
     global rgb_branchField, rgb_leafField, rgb_branchField
     cmds.colorSliderGrp('rgb_branchField', label='Branches', rgb=(0.43, 0.23, 0.11) )
     cmds.colorSliderGrp('rgb_leafField', label='Leaves', rgb=(0, 1, 0) )
     cmds.colorSliderGrp('rgb_blossomField',  label='Blossoms', rgb=(1, 0, 0) )
 
-    #cmds.floatSliderGrp("Radius Atenuation", l="radius_atenuation: ", v=1, cw3=[92,40,788], min=0, max=1, fmx=1, f=True, pre=2, ann='This number will multiply the length of the next segment, recursively')
-
+    #cmds.floatSliderGrp("Radius Atenuation", l="radius_atenuation: ", v=1, cw3=[92,40,788], min=0, max=1,
+    # fmx=1, f=True, pre=2, ann='This number will multiply the length of the next segment, recursively')
 
         #--- Create Geometry / Clean Scene ---#
     cmds.rowColumnLayout( numberOfColumns=3, columnWidth=[(1,196), (2,10), (3,196)], parent=mInterpret)
@@ -189,10 +211,13 @@ def createUI():
     cmds.separator(h=5, st='none')
 
     #--- ANIMATION LAYOUT ---#
-    kAnimation = cmds.frameLayout(label = "Animation Settings", collapsable=True, cl=True, mw = 10, mh = 10)
+    kAnimation = cmds.frameLayout(label = "Animation Settings", collapsable=True, cl=True, mw = 10,
+        mh = 10)
     cmds.rowColumnLayout( numberOfColumns=1, columnWidth=[(1,406)], parent=kAnimation)
-    cmds.intSliderGrp('keyEvery', l='K every (frames):', v=0, cw3=[92,30,798], min=1, max=10, fmx=20, f=True)
-    cmds.intSliderGrp('angleVar', l='Angle Oscilation: ', v=0, cw3=[92,30,278], min=1, max=10, fmx=20, f=True)
+    cmds.intSliderGrp('keyEvery', l='K every (frames):', v=0, cw3=[92,30,798], min=1, max=10, fmx=20,
+        f=True)
+    cmds.intSliderGrp('angleVar', l='Angle Oscilation: ', v=0, cw3=[92,30,278], min=1, max=10, fmx=20,
+        f=True)
 
         #--- Add Animation / Clear Keyframes ---#
     cmds.rowColumnLayout( numberOfColumns=3, columnWidth=[(1,196), (2,10), (3,196)], parent=kAnimation)
@@ -220,13 +245,17 @@ def generateStringButtonAction(*pArgs):
     pAxiom = cmds.textField('axiomTextField', q=True, tx=True)
 
     pP = []
-    pP.append([str(cmds.intField('prodRuleProb1', q=True, v=True)),str(cmds.textField('prodRulePred1', q=True, tx=True)),str(cmds.textField('prodRuleSucc1', q=True, tx=True))])
+    pP.append([str(cmds.intField('prodRuleProb1', q=True, v=True)),str(cmds.textField('prodRulePred1',
+        q=True, tx=True)),str(cmds.textField('prodRuleSucc1', q=True, tx=True))])
     if cmds.checkBox('prodRuleCheckBox2', q=True, value=True) == True:
-        pP.append([str(cmds.intField('prodRuleProb2', q=True, v=True)),str(cmds.textField('prodRulePred2', q=True, tx=True)),str(cmds.textField('prodRuleSucc2', q=True, tx=True))])
+        pP.append([str(cmds.intField('prodRuleProb2', q=True, v=True)),str(cmds.textField('prodRulePred2',
+            q=True, tx=True)),str(cmds.textField('prodRuleSucc2', q=True, tx=True))])
     if cmds.checkBox('prodRuleCheckBox3', q=True, value=True) == True:
-        pP.append([str(cmds.intField('prodRuleProb3', q=True, v=True)),str(cmds.textField('prodRulePred3', q=True, tx=True)),str(cmds.textField('prodRuleSucc3', q=True, tx=True))])
+        pP.append([str(cmds.intField('prodRuleProb3', q=True, v=True)),str(cmds.textField('prodRulePred3',
+            q=True, tx=True)),str(cmds.textField('prodRuleSucc3', q=True, tx=True))])
     if cmds.checkBox('prodRuleCheckBox4', q=True, value=True) == True:
-        pP.append([str(cmds.intField('prodRuleProb4', q=True, v=True)),str(cmds.textField('prodRulePred4', q=True, tx=True)),str(cmds.textField('prodRuleSucc4', q=True, tx=True))])
+        pP.append([str(cmds.intField('prodRuleProb4', q=True, v=True)),str(cmds.textField('prodRulePred4',
+            q=True, tx=True)),str(cmds.textField('prodRuleSucc4', q=True, tx=True))])
 
     pDepth = cmds.intSliderGrp('depthIntField', q=True, v=True)
 
@@ -256,7 +285,8 @@ def createGeometryButtonAction(*pArgs):
         createBranchShader(rgb_branch)
         createLeafShader(rgb_leaf)
         createBlossomShader(rgb_blossom)
-        createGeometry(LStringVar, pRad, pStep, pAngle, subDivs, length_atenuation, radius_atenuation, rgb_blossom, rgb_leaf, rgb_branch)
+        createGeometry(LStringVar, pRad, pStep, pAngle, subDivs, length_atenuation, radius_atenuation,
+            rgb_blossom, rgb_leaf, rgb_branch)
 
 #--- CLEAN ACTION ---#
 def cleanSceneButtonAction(*pArgs):
--- script/startScript.py
+++ script/startScript.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-''' DISCLAIMER: This startScript.py file is adapted from Jared Auty's scripting project "L-System Plant Generator". All the
-    copyrights belongs him, just on this file, of course.
+''' DISCLAIMER: This startScript.py file is adapted from Jared Auty's scripting project
+"L-System Plant Generator". All the copyrights belongs him, just on this file, of course.
      
     You can access his source code here: 
 
@@ -14,20 +14,23 @@
 
     Start Up Script    <startScript.py>
 
-Select everything and paste it into a new maya python scripting window. Then run it and follow the instructions
+Select everything and paste it into a new maya python scripting window.
+Then run it and follow the instructions.
 '''
 
 import maya.cmds as cmds
 
 class findPathWindow:
     '''
-    This class is responsible for managing the module-fininding window. This window is run when the script can't find the modules it needs.
-    It asks the user to locate the modules. The user-inputted path is then saved in python's temporary system paths. This means that next 
-    time it tries to load the modules it will look in the right path
+    This class is responsible for managing the module-fininding window. This window is run when the script
+    can't find the modules it needs. It asks the user to locate the modules. The user-inputted path is
+    then saved in python's temporary system paths. This means that next time it tries to load the modules
+    it will look in the right path
     '''
     def __init__(self):
         '''
-         This function initialises all the needed values and creates the GUI. It is run automatically when a new instance of the class is run.
+         This function initialises all the needed values and creates the GUI. It is run automatically when
+         a new instance of the class is run.
         '''
         self.fileName = []
         self.fileType = []
@@ -35,10 +38,13 @@ class findPathWindow:
         self.loadDirWin = cmds.window(title='L-System Interpreter', sizeable=False)
         self.loadDirForm = cmds.formLayout(numberOfDivisions=100)
         self.loadDirColumns = cmds.columnLayout(parent=self.loadDirForm, adjustableColumn=True, rowSpacing=5)
-        self.loadDirText = cmds.text(parent=self.loadDirColumns, font='boldLabelFont', label='Welcome to Ramon\'s L-System Interpreter\n\nTo start using the script simply browse to the directory containing the modules and then press \'Continue\'\n\n|\n|\nv')
+        self.loadDirText = cmds.text(parent=self.loadDirColumns, font='boldLabelFont',
+            label='Welcome to Ramon\'s L-System Interpreter\n\nTo start using the script simply browse tothe directory containing the modules and then press \'Continue\'\n\n|\n|\nv')
         self.loadDirPath = cmds.textField(parent = self.loadDirColumns)
-        self.loadDirBrowse= cmds.button(parent = self.loadDirColumns, label='Browse', command=self.openFile)
-        self.loadDirLoad = cmds.button(parent = self.loadDirColumns, label ='Continue', command=self.appendPath)
+        self.loadDirBrowse= cmds.button(parent = self.loadDirColumns, label='Browse',
+            command=self.openFile)
+        self.loadDirLoad = cmds.button(parent = self.loadDirColumns, label ='Continue',
+            command=self.appendPath)
         #---Make sure the colums layout stretches with the window---#
         cmds.formLayout(self.loadDirForm, edit=True, attachForm=[
             (self.loadDirColumns, 'top', 20),
@@ -48,26 +54,30 @@ class findPathWindow:
             ])
     def appendPath(self, *args):
         '''
-        This function queries the text field and puts the path in the system paths. It then runs the 'closeAll' function
+        This function queries the text field and puts the path in the system paths.
+        It then runs the 'closeAll' function
         '''
-        self.fileName = cmds.textField(self.loadDirPath, query=True, text=True)    # Query textfield and save in self.fileName
-        sys.path.append(self.fileName)    # Append to system paths
-        if self.fileName:    # If the fileName is not empty
+        # Query textfield and save in self.fileName
+        self.fileName = cmds.textField(self.loadDirPath, query=True, text=True) 
+        sys.path.append(self.fileName) # Append to system paths
+        if self.fileName: # If the fileName is not empty
             self.closeAll()
       
     def openFile(self, *args):
         '''
-        This opens the file browser and takes the path and puts it in the text field. This is run when the Browse button is pressed.
+        This opens the file browser and takes the path and puts it in the text field.
+        This is run when the Browse button is pressed.
         '''
-        self.fileName = cmds.fileDialog2(fileMode=2, caption="Import Image")    # Open the file browser
-        cmds.textField(self.loadDirPath, edit=True, text=str(self.fileName[0]))    # Put path in text field
+        self.fileName = cmds.fileDialog2(fileMode=2, caption="Import Image") # Open the file browser
+        cmds.textField(self.loadDirPath, edit=True, text=str(self.fileName[0])) # Put path in text field
       
     def closeAll(self, *args):
         '''
-        This function closes the window and starts up the main GUI. This is run when the Continue button is pressed.
+        This function closes the window and starts up the main GUI.
+        This is run when the Continue button is pressed.
         '''
-        cmds.deleteUI(self.loadDirWin, window=True)    # Close window
-        gui.createUI()    # Start UI
+        cmds.deleteUI(self.loadDirWin, window=True) # Close window
+        gui.createUI() # Start UI
 
 
 class errorWindow:
@@ -76,15 +86,19 @@ class errorWindow:
     '''
     def __init__(self, message):
         '''
-        This function creates the window. This is run when a new instance of the class is created. When creating a new instance
-        a string called message is passed in, this is then displayed on the window
+        This function creates the window. This is run when a new instance of the class is created.
+        When creating a new instance a string called message is passed in, this is then displayed on the
+        window
         '''
         self.win = cmds.window(title='Error')
         self.form = cmds.formLayout(numberOfDivisions=100, w=300, h=100)
         self.text = cmds.text(label=message)
         self.okButton = cmds.button(label='OK', command=self.close)
-        cmds.formLayout(self.form, edit=True,attachForm=[[self.text, 'left', 0], [self.text, 'right', 0], [self.okButton, 'left', 0], [self.okButton, 'right', 0]])
-        cmds.formLayout(self.form, edit=True,attachPosition=[[self.text, 'top', 0, 0], [self.text, 'bottom', 0, 70], [self.okButton, 'top', 0, 70], [self.okButton, 'bottom', 0, 100]])
+        cmds.formLayout(self.form, edit=True,attachForm=[[self.text, 'left', 0], [self.text, 'right', 0],
+            [self.okButton, 'left', 0], [self.okButton, 'right', 0]])
+        cmds.formLayout(self.form, edit=True,attachPosition=[[self.text, 'top', 0, 0],
+            [self.text, 'bottom', 0, 70], [self.okButton, 'top', 0, 70],
+            [self.okButton, 'bottom', 0, 100]])
         cmds.showWindow(self.win)
     def close(self, *args):
         '''
@@ -92,13 +106,13 @@ class errorWindow:
         '''
         cmds.deleteUI(self.win)
 
-try:    # If a gui is already open close it
+try: # If a gui is already open close it
     if cmds.window(win.window, exists=True):
         cmds.deleteUI(win.window, window=True)
 except:
     pass
 
-try:    # Try to import all the modules and then start the UI
+try: # Try to import all the modules and then start the UI
     import maya.cmds as cmds
     import random
     import math
@@ -113,7 +127,7 @@ try:    # Try to import all the modules and then start the UI
     plantNumber = 1
     gui.createUI()
 
-except:    # If the above fails create an instance of the findPathWindow to try to find the modules
+except: # If the above fails create an instance of the findPathWindow to try to find the modules
     import sys
     load = findPathWindow()
     cmds.showWindow(load.loadDirWin)
